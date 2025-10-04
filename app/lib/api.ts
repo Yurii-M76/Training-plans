9bd0b89b5cc09e4a343f4cde7cc470195de7aba6
Yurii M <ispoyar@yandex.ru>
2025-10-04 14:50:46 +0300

    added api to utils

---
+++ app/lib/api.ts
@@ -0,0 +1,19 @@
+export const checkResponse = <T>(res: Response): Promise<T> =>
+  res.ok ? res.json() : res.json().then((err) => Promise.reject(err));
+
+const apiUrl = "https://t-core.fit-hub.pro";
+
+export const findPlansFromApi = async <T>(path: string): Promise<T> => {
+  try {
+    const response = await fetch(`${apiUrl}/${path}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      } as HeadersInit,
+    });
+    return await checkResponse<T>(response);
+  } catch (error) {
+    console.error(`Request failed (${path} find all):`, error);
+    return Promise.reject(error);
+  }
+};
