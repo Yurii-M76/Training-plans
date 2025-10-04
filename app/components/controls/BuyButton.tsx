93f2416bd9639adceea169088b7708b15f393f2f
Yurii M <ispoyar@yandex.ru>
2025-10-04 14:52:07 +0300

    added animation to buy-button

--- app/components/controls/BuyButton.tsx
+++ app/components/controls/BuyButton.tsx
@@ -2,7 +2,7 @@ export const BuyButton = () => {
   return (
     <button
       type="button"
-      className="xs:h-[63px] flex h-[55px] w-full cursor-pointer items-center justify-center rounded-[20px] bg-[var(--accent)] text-[18px] leading-[1.3] font-bold text-[#191e1f] md:h-[66px] md:w-[352px] md:text-[20px]"
+      className="xs:h-[63px] flex h-[55px] w-full cursor-pointer items-center justify-center rounded-[20px] bg-[var(--accent)] text-[18px] leading-[1.3] font-bold text-[#191e1f] not-hover:animate-pulse md:h-[66px] md:w-[352px] md:text-[20px]"
     >
       Купить
     </button>
