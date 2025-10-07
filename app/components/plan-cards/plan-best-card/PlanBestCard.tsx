b84adebfcdacab39cd266b6e69df32ce192cb1c3
Yurii M <ispoyar@yandex.ru>
2025-10-07 16:07:07 +0300

    update PlanCard & PlanBestCard

--- app/components/plan-cards/plan-best-card/PlanBestCard.tsx
+++ app/components/plan-cards/plan-best-card/PlanBestCard.tsx
@@ -1,52 +1,81 @@
 import { FC } from "react";
 import { IPlan } from "@/app/interfaces";
-import { numberWithSpaces } from "@/app/lib/utils";
 import { Sale } from "../../sale/Sale";
+import { cn, numberWithSpaces } from "@/app/lib/utils";
 
-export const PlanBestCard: FC<IPlan> = ({
-  period,
-  price = 0,
-  full_price = 0,
-  sale = 0,
-  text,
-}) => {
+export const PlanBestCard: FC<{
+  values: IPlan;
+  sale: number;
+  isSale: boolean;
+  isSelected: boolean;
+}> = ({ values, sale, isSale, isSelected, ...props }) => {
+  const { period, price, full_price, text } = values;
+  const fullPriceFormat = numberWithSpaces(full_price);
+
   return (
-    <div className="xs:h-[131px] xs:p-[18px_31px_20px_28px] xs:w-[343px] relative flex h-[118px] w-[288px] rounded-[20px] border-[2px] border-[var(--accent)] bg-[var(--bg-card)] p-[18px_9px_20px_18px] md:h-[190px] md:w-full md:rounded-[34px] md:p-[34px_80px_30px_122px]">
-      <div className="xs:right-[62px] absolute -top-[2px] right-[50px] md:left-[49px]">
-        <Sale value={sale} />
-      </div>
+    <>
+      <input
+        type="radio"
+        id={values.id}
+        value={values.id}
+        className="hidden"
+        {...props}
+      />
+      <label
+        htmlFor={values.id}
+        className={cn(
+          "xs:h-[131px] xs:p-[18px_31px_20px_28px] xs:w-[343px] relative flex h-[118px] w-[288px] cursor-pointer overflow-hidden rounded-[20px] border-[2px] bg-[var(--bg-card)] p-[18px_9px_20px_18px] shadow-amber-400 hover:shadow-2xs md:h-[190px] md:w-full md:rounded-[34px] md:p-[34px_80px_30px_122px]",
+          isSelected ? "border-[var(--accent)]" : "border-[var(--outline)]",
+        )}
+      >
+        <div
+          className={cn(
+            "xs:right-[62px] absolute -top-[2px] right-[50px] md:left-[49px]",
+            !isSale &&
+              "-translate-y-10 opacity-0 transition-all duration-350 ease-linear",
+          )}
+        >
+          <Sale value={sale} />
+        </div>
 
-      <span className="xs:text-[16px] absolute top-[6px] right-[14px] text-[13px] leading-[1.3] font-medium tracking-[0.03em] text-[var(--accent)] md:top-[10px] md:right-[20px] md:text-[22px]">
-        хит!
-      </span>
+        <span className="xs:text-[16px] absolute top-[6px] right-[14px] text-[13px] leading-[1.3] font-medium tracking-[0.03em] text-[var(--accent)] md:top-[10px] md:right-[20px] md:text-[22px]">
+          хит!
+        </span>
 
-      <div className="flex h-full w-full items-center justify-between gap-[30px] md:gap-[40px]">
-        <div className="xs:h-[91px] flex h-[78px] shrink-0 flex-col justify-between md:h-[126px] md:items-center">
-          <span className="xs:text-[18px] text-[16px] leading-[1.2] font-medium md:text-[26px]">
-            {period}
-          </span>
-
-          <div className="flex w-fit flex-col">
-            <span className="xs:text-[34px] inline-flex justify-center text-[30px] leading-[1] font-semibold text-[var(--accent)] md:text-[50px]">
-              {price} ₽
+        <div className="flex h-full w-full items-center justify-between gap-[30px] md:gap-[40px]">
+          <div className="xs:h-[91px] flex h-[78px] shrink-0 flex-col justify-between md:h-[126px] md:items-center">
+            <span className="xs:text-[18px] text-[16px] leading-[1.2] font-medium md:text-[26px]">
+              {period}
             </span>
-            <div className="flex justify-end">
-              <span className="xs:text-[16px] text-[14px] leading-[1.2] text-[#919191] line-through md:text-[24px]">
-                {numberWithSpaces(full_price)} ₽
+
+            <div className="flex w-fit flex-col">
+              <span className="xs:text-[34px] inline-flex justify-center text-[30px] leading-[1] font-semibold text-[var(--accent)] md:text-[50px]">
+                {isSale ? price : fullPriceFormat} ₽
               </span>
+              <div className="flex justify-end">
+                <span
+                  className={cn(
+                    "xs:text-[16px] text-[14px] leading-[1.2] text-[#919191] line-through md:text-[24px]",
+                    !isSale &&
+                      "opacity-0 transition-opacity duration-350 ease-linear",
+                  )}
+                >
+                  {fullPriceFormat} ₽
+                </span>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="xs:w-[111px] flex w-[120px] items-center overflow-hidden md:h-[62px] md:w-[328px]">
-          <p className="text-[14px] leading-[1.3] not-md:hidden md:text-[16px]">
-            {text}
-          </p>
-          <p className="text-[14px] leading-[1.3] md:hidden md:text-[16px]">
-            Всегда быть в форме
-          </p>
+          <div className="xs:w-[111px] flex w-[120px] items-center overflow-hidden md:h-[62px] md:w-[328px]">
+            <p className="text-[14px] leading-[1.3] not-md:hidden md:text-[16px]">
+              {text}
+            </p>
+            <p className="text-[14px] leading-[1.3] md:hidden md:text-[16px]">
+              Всегда быть в форме
+            </p>
+          </div>
         </div>
-      </div>
-    </div>
+      </label>
+    </>
   );
 };
