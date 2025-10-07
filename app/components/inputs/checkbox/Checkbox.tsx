a38241975e7cf8e44c65fb7330474fe8317e9107
Yurii M <ispoyar@yandex.ru>
2025-10-07 16:03:18 +0300

    transferring a checkbox component

--- app/components/inputs/checkbox/Checkbox.tsx
+++ app/components/inputs/checkbox/Checkbox.tsx
@@ -7,16 +7,22 @@ interface ICheckbox {
   isNotConfirmed?: boolean;
 }
 
-export const Checkbox = ({ id, children, isNotConfirmed }: ICheckbox) => {
+export const Checkbox = ({
+  id,
+  children,
+  isNotConfirmed,
+  ...props
+}: ICheckbox) => {
   return (
-    <div className="mt-[5px] flex w-fit items-center justify-center gap-[12px]">
+    <div className="mt-[5px] flex w-fit items-start justify-center gap-[12px]">
       <input
+        id={id}
+        type="checkbox"
         className={cn(
           "disabled: relative size-[30px] shrink-0 appearance-none rounded-[4px] border-2 border-[#606566] bg-size-[22px_16px] bg-center bg-no-repeat checked:bg-[url(/icons/check.svg)] md:size-[32px]",
           isNotConfirmed && "border-red-600",
         )}
-        type="checkbox"
-        id={id}
+        {...props}
       />
       <label htmlFor={id}>{children}</label>
     </div>
