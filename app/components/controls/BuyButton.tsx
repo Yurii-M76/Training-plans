de2e0df9f6bb422a3343a3450b712bda2eca16f0
Yurii M <ispoyar@yandex.ru>
2025-10-07 16:03:47 +0300

    added onClick to buy button

--- app/components/controls/BuyButton.tsx
+++ app/components/controls/BuyButton.tsx
@@ -1,8 +1,9 @@
-export const BuyButton = () => {
+export const BuyButton = ({ onClick }: { onClick: () => void }) => {
   return (
     <button
-      type="button"
+      type="submit"
       className="xs:h-[63px] flex h-[55px] w-full cursor-pointer items-center justify-center rounded-[20px] bg-[var(--accent)] text-[18px] leading-[1.3] font-bold text-[#191e1f] not-hover:animate-pulse md:h-[66px] md:w-[352px] md:text-[20px]"
+      onClick={onClick}
     >
       Купить
     </button>
