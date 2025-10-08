8426b086b5745d4da7a4973e51de7569966d5240
Yurii M <ispoyar@yandex.ru>
2025-10-08 11:28:45 +0300

    added allowedDevOrigins to next.config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ["https://training-plans.vercel.app/api/*"],
 };
 
 export default nextConfig;
