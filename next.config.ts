ee1b1321d17958179e457af58e984e63215e2e55
Yurii M <ispoyar@yandex.ru>
2025-10-08 15:02:40 +0300

    update next.config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ["https://training-plans.vercel.app/api/*"],
 };
 
 export default nextConfig;
